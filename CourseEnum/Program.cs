d378db216dea2e642b458909cb4b55e9b46bcc39
Emerson <emersonlapa@outlook.com>
2020-10-25 23:09:07 -0300

    Upload

---
+++ CourseEnum/Program.cs
@@ -0,0 +1,20 @@
+﻿using CourseEnum.Entities;
+using System;
+using CourseEnum.Entities.Enums;
+
+namespace CourseEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1800,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+        Console.WriteLine(order);
+        }
+    }
+}
